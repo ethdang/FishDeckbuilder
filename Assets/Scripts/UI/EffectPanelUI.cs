f195b4c80fc22efce8c2c7a3f0fee25d51e41f49
ethdang <ethan.g.dang@gmail.com>
2026-07-29 17:02:18 -0700

    making upcoming

--- Assets/Scripts/UI/EffectPanelUI.cs
+++ Assets/Scripts/UI/EffectPanelUI.cs
@@ -1,12 +1,47 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class EffectPanelUI : MonoBehaviour
 {
+    public List<GameObject> activeCurrentObjs = new();
+    public List<GameObject> activeUpcomingObjs = new();
+
     [SerializeField] private GameObject currentEffectPrefab;
     [SerializeField] private GameObject upcomingEffectPrefab;
 
     [SerializeField] private RectTransform currentEffectParent;
     [SerializeField] private RectTransform upcomingEffectParent;
 
+    private TurnManager turnManager;
+
+    void Awake()
+    {
+        turnManager = FindFirstObjectByType<TurnManager>();
+    }
+
+    public void UpdateUpcomingEffects()
+    {
+        foreach (GameObject obj in activeUpcomingObjs)
+            Destroy(obj);
+
+        activeCurrentObjs.Clear();
+
+        foreach (PendingEffect effect in turnManager.pendingEffects)
+        {
+            GameObject newUpcomingEffect = Instantiate(upcomingEffectPrefab, upcomingEffectParent);
+
+            activeUpcomingObjs.Add(newUpcomingEffect);
+
+            GameObject delayTextObj = newUpcomingEffect.GetComponentInChildren<DelayText>().gameObject;
+            TMP_Text delayText = delayTextObj.GetComponent<TMP_Text>();
+
+            delayText.text = effect.turnsRemaining.ToString();
+
+            GameObject effectTextObj = newUpcomingEffect.GetComponentInChildren<EffectText>().gameObject;
+            TMP_Text effectText = effectTextObj.GetComponent<TMP_Text>();
+
+            effectText.text = effect.effect.ToString();
+        }
+    }
 }
